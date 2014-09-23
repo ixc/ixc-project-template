a5f8bd0fb0eeace6d01e0167b9270a788f20fb5d
Tai Lee <real.human@mrmachine.net>
2014-09-23 23:43:19 +1000

    Add missing comma.

--- djangosite/settings/base.py
+++ djangosite/settings/base.py
@@ -248,7 +248,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.core.context_processors.tz',
-    'django.contrib.messages.context_processors.messages'
+    'django.contrib.messages.context_processors.messages',
 
     # Extras.
     'django.core.context_processors.request',
