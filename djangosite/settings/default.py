9ebbc161d8aea5b2ce6329680f86701942ce0d3b
Tai Lee <real.human@mrmachine.net>
2015-05-29 17:56:29 +1000

    Remove reference to vestigial local settings module template.

--- djangosite/settings/default.py
+++ djangosite/settings/default.py
@@ -10,7 +10,7 @@ except ImportError:
 You MUST **review and uncomment** the appropriate environment specific
 configuration in the local settings module:
 
-    (venv)$ cp {path}/local.tmpl.py {path}/local.py
+    (venv)$ cp {path}/local_sample.py {path}/local.py
     (venv)$ $EDITOR {path}/local.py
 """
     raise ImproperlyConfigured(message.format(path=path))
