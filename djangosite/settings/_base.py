d9275ff474dececc61a49796c7c833b18c871eda
Tai Lee <real.human@mrmachine.net>
2016-03-22 09:50:33 +1100

    Remove stray unicode text.

--- djangosite/settings/_base.py
+++ djangosite/settings/_base.py
@@ -506,7 +506,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'  # Default: django.test.runner.D
 
 NOSE_ARGS = [
     '--logging-clear-handlers',  # Clear all other logging handlers
-    '--nocapture',  # Don’t capture stdout
+    '--nocapture',  # Don't capture stdout
     '--nologcapture',  # Disable logging capture plugin
     # '--processes=-1',  # Automatically set to the number of cores
     '--with-progressive',  # See https://github.com/erikrose/nose-progressive
