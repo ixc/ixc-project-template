895b5bc782fa59faf171ed3859091b2b19471896
Tai Lee <real.human@mrmachine.net>
2016-03-22 11:48:48 +1100

    Update test settings module.

--- project_name/tests/settings.py
+++ project_name/tests/settings.py
@@ -1,17 +1,37 @@
-"""
-Test settings for ``{{ project_name }}`` project.
-"""
+from djangosite.settings.calculated import *
 
-from djangosite.settings.local_sample import *
+# DJANGO ######################################################################
 
-DATABASES = {
-    'default': {
-        'ATOMIC_REQUESTS': True,
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-    }
-}
+# Always use the same database, not only when running tests.
+DATABASES['default'].update({
+    'NAME': 'test_{{ project_name }}',
+    'TEST': {
+        'NAME': 'test_{{ project_name }}',
+        'SERIALIZE': False,  # Don't serialize the database to speed up tests.
+    },
+})
 
-INSTALLED_APPS += (
-    '{{ project_name }}.tests',
-)
+INSTALLED_APPS += ('{{ project_name }}.tests', )
+
+# Use fast/insecure password hasher to speed up tests.
+PASSWORD_HASHERS = ('django.contrib.auth.hashers.MD5PasswordHasher', )
+
+# Use cached template loader to speed up tests.
+TEMPLATES_DJANGO['OPTIONS']['loaders'] = [(
+    'django.template.loaders.cached.Loader',
+    TEMPLATES_DJANGO['OPTIONS']['loaders']
+)]
+
+# COMPRESSOR ##################################################################
+
+# Don't compile compress CSS and JS to speed up tests.
+COMPRESS_ENABLED = False
+COMPRESS_PRECOMPILERS = ()
+
+# TRAVIS ######################################################################
+
+if 'TRAVIS' in os.environ:
+    DATABASES['default']['USER'] = 'postgres'
+
+    # Disable progressive plugin, which doesn't work properly on Travis.
+    NOSE_ARGS.remove('--with-progressive')
