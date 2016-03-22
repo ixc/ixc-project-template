c874891786f57b6fbba2dd7844b2fa1759e54272
Tai Lee <real.human@mrmachine.net>
2016-03-22 16:30:59 +1100

    Add setup `script-dev.sh` and `gulpfile.js` files for local development.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp  = require('gulp');
+var child = require('child_process');
+
+var main = null;
+
+function pipe(result) {
+	result.stderr.on('data', function(data) {
+		process.stdout.write(data.toString());
+	});
+	result.stdout.on('data', function(data) {
+		process.stdout.write(data.toString());
+	});
+}
+
+gulp.task('default', ['setup', 'main', 'watch']);
+
+gulp.task('watch', function() {
+	gulp.watch('./**/*.py', ['main']);
+	gulp.watch('./bower.json', ['setup']);
+	gulp.watch('./package.json', ['setup']);
+	gulp.watch('./setup.py', ['setup']);
+});
+
+gulp.task('setup', function() {
+	return child.spawnSync('setup-dev.sh');
+});
+
+gulp.task('main', function() {
+	if (main) {
+		main.kill();
+	}
+	main = child.spawn('gunicorn.sh');
+	pipe(main);
+});
