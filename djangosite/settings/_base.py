8e6d15a0330bedcba61904da8e5075d2c49fc94b
Tai Lee <real.human@mrmachine.net>
2016-03-24 02:11:00 +1100

    Get email settings from environment.

--- djangosite/settings/_base.py
+++ djangosite/settings/_base.py
@@ -75,6 +75,9 @@ DATABASES = {
 }
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
