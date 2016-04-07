a2e66479fe213e39449b06472ced089a15f3884c
Tai Lee <real.human@mrmachine.net>
2016-04-07 16:47:28 +1000

    Work around inconsistent behaviour with Docker for Mac file sharing.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,4 +3,10 @@
 set -e
 
 setup-user.sh "${PROJECT_NAME}" "${PROJECT_DIR}/var"
+
+# Work around inconsistent behaviour with Docker for Mac file sharing.
+if [[ $(stat -c '%u' "${PROJECT_DIR}/var") == 0 ]]; then
+	exec gosu "${PROJECT_NAME}" entrypoint-django.sh "$@"
+fi
+
 exec entrypoint-gosu-dir.sh "${PROJECT_DIR}/var" entrypoint-django.sh "$@"
