050aee0ab607fec3c862500237355c7860fbae44
Tai Lee <real.human@mrmachine.net>
2016-04-08 14:16:07 +1000

    Fix renamed script references.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -6,7 +6,7 @@ setup-user.sh "${PROJECT_NAME}" "${PROJECT_DIR}/var"
 
 # Work around inconsistent behaviour with Docker for Mac file sharing.
 if [[ $(stat -c '%u' "${PROJECT_DIR}/var") == 0 ]]; then
-	exec gosu "${PROJECT_NAME}" entrypoint-django.sh "$@"
+	exec gosu "${PROJECT_NAME}" setup-django-env.sh "$@"
 fi
 
-exec entrypoint-gosu-dir.sh "${PROJECT_DIR}/var" entrypoint-django.sh "$@"
+exec gosu-dir.sh "${PROJECT_DIR}/var" setup-django-env.sh "$@"
