3d9bae52a761b3634b925941d5c9191eb984480b
Tai Lee <real.human@mrmachine.net>
2016-04-08 13:57:26 +1000

    Simplify gulpfile. Just chain setup at the start of the main task, and restart it on all changes.
    The setup scripts should be smart enough to not do anything if they
    don't need to.
    
    Inherit stdio, so we can see what the main script is doing when it is
    restarted.

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,22 @@ var child = require('child_process');
 
 var main = null;
 
-gulp.task('default', ['setup', 'main', 'watch']);
+gulp.task('default', ['main', 'watch']);
 
 gulp.task('watch', function() {
 	gulp.watch('./**/*.py', ['main']);
-	gulp.watch('./bower.json', ['setup']);
-	gulp.watch('./package.json', ['setup']);
-	gulp.watch('./requirements*.txt', ['setup']);
-	gulp.watch('./setup.py', ['setup']);
-});
-
-gulp.task('setup', function() {
-	child.spawnSync('setup-local-dev.sh', {stdio: 'inherit'});
+	gulp.watch('./bower.json', ['main']);
+	gulp.watch('./package.json', ['main']);
+	gulp.watch('./requirements*.txt', ['main']);
+	gulp.watch('./setup.py', ['main']);
 });
 
 gulp.task('main', function() {
 	if (main) {
 		main.kill();
 	}
-	main = child.spawn('supervisor.sh', {stdio: 'inherit'});
+	main = child.spawn(
+		'setup-local-env.sh',
+		['migrate.sh', 'supervisor.sh'],
+		{stdio: 'inherit'});
 });
