53198585313216dd1b770380c0bfd8159f591fe7
ayeowch <ayeowch@gmail.com>
2016-04-08 15:51:44 +1000

    Update entry point command and use `expose` instead of `ports` for internal services.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -3,4 +3,4 @@
 set -e
 
 setup-user.sh "${PROJECT_NAME}" "${PROJECT_DIR}/var"
-exec entrypoint-gosu-dir.sh "${PROJECT_DIR}/var" entrypoint-django.sh "$@"
+exec gosu-dir.sh "${PROJECT_DIR}/var" setup-django-env.sh "$@"
