14c163feac0451ff3a766c4c6b498b889fbf0ffa
Tai Lee <real.human@mrmachine.net>
2016-04-13 01:03:30 +1000

    Refactor `startproject.sh` script.
    * Accept destination directory as second argument, instead of template
      path or URL.
    
    * Hard code template URL.
    
    * Prompt for confirmation before making changes to global Python
      environment, e.g. automatically installing Pip or Virtualenv, and
      after explaining what the script will do (and before doing anything).
    
    * Always install Django into a temporary virtualenv and cleanup
      afterwards, to avoid conflicts with the users global or current
      environment.
    
    * Abort if Python or curl/wget is not available.
    
    * Prompt to initialize a Git repository and create an initial commit,
      when we detect that Git is installed. The default answer is "Y".
    
    * Print Docker runtime instructions on completion, and links to docker
      quick start and manual setup guides.

--- startproject.sh
+++ startproject.sh
@@ -2,58 +2,133 @@
 
 set -e
 
-PROJECT="$1"
-MODULE="${PROJECT//[^0-9A-Za-z]/_}"
-TEMPLATE="${2:-https://github.com/ixc/ixc-project-template/archive/django-icekit.zip}"
+# Use "-" as separator for project (directory) name. Use "_" as separator for
+# module name.
+PROJECT_NAME="${1//[^0-9A-Za-z]/-}"
+PACKAGE_NAME="${1//[^0-9A-Za-z]/_}"
 
-if [[ -d "${PROJECT}" ]]; then
-	echo "Project directory '${PWD}/${PROJECT}' already exists. Abort."
+# Use project name as default destination directory.
+DEST_DIR="${2-:${PROJECT_NAME}}"
+
+# Hard code template URL.
+TEMPLATE="https://github.com/ixc/ixc-project-template/archive/django-icekit.zip"
+
+# Abort if Python is not available.
+if [[ -z $(which python) ]]; then
+	echo 'Python is not available. Aborting.'
 	exit 1
 fi
 
-if [[ -z $(which django-admin.py) ]]; then
-	echo 'Django is missing. Install.'
-	pip install Django
+# Abort if the destination directory already exists.
+if [[ -d "${DEST_DIR}" ]]; then
+	echo "Project directory '${DEST_DIR}' already exists. Aborting."
+	exit 1
 fi
 
-echo "Creating project directory '${PWD}/${PROJECT}'"
-mkdir "${PWD}/${PROJECT}"
+cat <<EOF
 
-echo "Creating project from template '${TEMPLATE}'"
+This script will create a new ICEkit project in directory '${DEST_DIR}' and
+with Python package name '${PACKAGE_NAME}'."
+
+It might need to install 'pip' and 'virtualenv' into your global environment,
+and will prompt for confirmation if it does.
+
+EOF
+
+read -p 'Press CTRL-C to abort or any other key to continue...'
+echo
+
+# Install Pip, if we need to.
+if [[ -z $(which pip) ]]; then
+	echo 'Pip is not available. Installing.'
+	read -p 'Press CTRL-C to abort or any other key to continue...'
+	echo
+
+	if [[ -n $(which curl) ]]; then
+		curl -L https://bootstrap.pypa.io/get-pip.py | python
+	elif [[ -n $(which wget) ]]; then
+		wget https://bootstrap.pypa.io/get-pip.py | python
+	else
+		cat <<EOF
+
+Neither 'curl' or 'wget' is available. Please install one or the other and try
+again. Aborting.
+
+EOF
+		exit 1
+	fi
+fi
+
+# Install virtualenv, if we need to.
+if [[ -z $(which virtualenv) ]]; then
+	echo
+	echo 'Virtualenv is not available. Installing.'
+	echo
+	read -p 'Press CTRL-C to abort or any other key to continue...'
+	echo
+	pip install virtualenv
+fi
+
+# Install Django into a temporary virtualenv.
+echo 'Installing Django into temporary virtualenv.'
+virtualenv "/tmp/${PROJECT_NAME}-venv"
+source "/tmp/${PROJECT_NAME}-venv/bin/activate"
+pip install Django
+
+# Create project directory, to silence `django-admin.py startproject` error
+# when specifying a destination directory.
+if [[ ! -d "${DEST_DIR}" ]]; then
+	echo "Creating project directory '${DEST_DIR}'."
+	mkdir -p "${DEST_DIR}"
+fi
+
+# Create project from template.
+echo "Creating project from template '${TEMPLATE}'."
 django-admin.py startproject \
 	-e json,ini,md,yml \
 	-n .coveragerc,base.html,Dockerfile \
 	--template="${TEMPLATE}" \
-	"${MODULE}" "${PROJECT}"
+	"${PACKAGE_NAME}" \
+	"${DEST_DIR}"
 
-cd "${PROJECT}"
+# Remove temporary virtualenv, if it exists.
+if [[ -d "/tmp/${PROJECT_NAME}-venv" ]]; then
+	echo 'Remove temporary virtualenv.'
+	deactivate
+	rm -rf "/tmp/${PROJECT_NAME}-venv"
+fi
 
-echo "Making '${PWD}/bin/*.sh' and ${PWD}/manage.py' executable."
+cd "${DEST_DIR}"
+
+echo "Making 'bin/*.sh' and 'manage.py' scripts executable."
 chmod 755 bin/*.sh manage.py
 
-echo "Initializing git repository and creating an initial commit."
-git init
-git add -A
-git commit -m 'Initial commit.'
+if [[ -n $(which git) ]]; then
+	echo
+	read -p 'Would you like to initialize a Git repository for your new project and create an initial commit? (Y/n) ' -n 1 -r
+	echo
+	if [[ "${REPLY:-y}" =~ ^[Yy]$ ]]; then
+		git init
+		git add -A
+		git commit -m 'Initial commit.'
+	fi
+fi
 
 cat <<EOF
 
 All done!
 
-Well, except for a few manual steps:
+If you have Docker installed, you can run your new ICEkit project immediately
+with:
 
-  * Add an 'origin' remote at 'git@github.com:{YOU}/${PROJECT}.git'.
-  * Create the '{YOU}/${PROJECT}' repository on GitHub.
-  * Enable the repository on Travis CI and add the 'DOCKER_PASSWORD' and
-    'PIP_INDEX_URL' environment variables in the repository settings.
-  * Push your initial commit.
+	$ cd ${DEST_DIR}
+	$ docker-compose up
 
-Then, you can get to work on your project! You might want to start with:
+Otherwise, check out our [Docker Quick Start][0] guide or [Manual Setup][1]
+guide to finish setting up your new ICEkit project.
 
-  * Add a 'LICENSE.txt' file - [MIT](http://choosealicense.com/licenses/mit/)
-  * Read the [contributing](docs/contributing.md) docs.
-  * Update the [docs](docs/index.md) (e.g. overview, installation and usage).
-  * Remove the 'Project Template' section (these instructions) from
-    'README.md'.
+[0]: https://github.com/ixc/django-icekit/docs/docker-quick-start.md
+[1]: https://github.com/ixc/django-icekit/docs/manual-setup-guide.md
 
 EOF
+fi
