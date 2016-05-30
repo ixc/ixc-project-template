6378f6cc98fd06d5fa196e6baf79b71a7125347c
Tai Lee <real.human@mrmachine.net>
2016-05-31 00:26:34 +1000

    Add default global and optional context.

--- djangosite/settings/_base.py
+++ djangosite/settings/_base.py
@@ -357,11 +357,16 @@ STATICFILES_FINDERS += ('compressor.finders.CompressorFinder', )
 # IXC_COMPRESSOR_REQUEST = False  # Default: True
 
 # A sequence of key/value tuples to be included in every generated context.
-IXC_COMPRESSOR_GLOBAL_CONTEXT = ()
+IXC_COMPRESSOR_GLOBAL_CONTEXT = (
+    ('base_change_form_template', 'admin/change_form.html'),
+    ('change_form_template', 'admin/change_form.html'),
+)
 
 # A sequence of key/value tuples, every combination of which will be combined
 # with the global context when generating contexts.
-IXC_COMPRESSOR_OPTIONAL_CONTEXT = ()
+IXC_COMPRESSOR_OPTIONAL_CONTEXT = (
+    ('base_change_form_template', 'admin/fluent_pages/page/base_change_form.html'),
+)
 
 # DYNAMIC FIXTURES ############################################################
 
